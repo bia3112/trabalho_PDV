8e86050c0b5dbb709983bc7d2e584ec6472b6ca1
Beatriz <beatriz.omori@edu.unipar.br>
2024-04-04 12:31:13 -0300

    criando o arquivo

---
+++ trabalhoPDV/src/main/java/br/unipar/trabalhoPDV/TrabalhoPDV.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.unipar.trabalhoPDV;
+
+/**
+ *
+ * @author Beatr
+ */
+public class TrabalhoPDV {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
