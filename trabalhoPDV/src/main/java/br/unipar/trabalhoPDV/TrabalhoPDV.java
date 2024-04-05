3823d194fb8d7039933e4eb809c4130c0c7c7750
Vini123t <111025380+Vini123t@users.noreply.github.com>
2024-04-05 09:15:59 -0300

    arrumando lista clientes

--- trabalhoPDV/src/main/java/br/unipar/trabalhoPDV/TrabalhoPDV.java
+++ trabalhoPDV/src/main/java/br/unipar/trabalhoPDV/TrabalhoPDV.java
@@ -1,17 +1,45 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+
 
 package br.unipar.trabalhoPDV;
 
 import br.unipar.trabalhoPDV.Util.EntityManagerUtil;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.SwingUtilities;
+import br.unipar.trabalhoPDV.panels.ListarCliente;
+import java.awt.event.*;
 
 /**
  *
  * @author Beatr
  */
-public class TrabalhoPDV {
+public class TrabalhoPDV  extends JFrame  {
+        setTitle("Menu");
+        setSize(600, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+         
+           // Criando a barra de menu 
+          JMenuBar menuBar = new JMenuBar();
+           //Menu de Clientes
+          JMenu menuCliente = new JMenu("Cliente");
+           //Itens do Menu de Clientes
+          JMenuItem ListaClientes = new JMenuItem("Lista Clientes");
+          
+          //Adiciona os menus itens no menu pai
+           menuCliente.add(ListaClientes);
+           //Adiciona o menu pai no menubar
+            menuBar.add(menuCliente);
+            
+            listarCliente.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                new ListarClienteFrame().setVisible(true);
+            }
+        });
+          
+          
 
     public static void main(String[] args) {
          SwingUtilities.invokeLater(new Runnable() {
@@ -19,5 +47,10 @@ public class TrabalhoPDV {
                    EntityManagerUtil.getEntityManagerFactory();
             }
         });
+    }
+     @Override
+    public void dispose() {
+        EntityManagerUtil.closeEntityManagerFactory();
+        super.dispose(); 
     }
 }
