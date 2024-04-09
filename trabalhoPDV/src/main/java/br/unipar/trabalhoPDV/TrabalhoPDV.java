65b9dd80970480b32187766e30a39bb622e389fa
Vini123t <111025380+Vini123t@users.noreply.github.com>
2024-04-09 20:26:55 -0300

    euto

--- trabalhoPDV/src/main/java/br/unipar/trabalhoPDV/TrabalhoPDV.java
+++ trabalhoPDV/src/main/java/br/unipar/trabalhoPDV/TrabalhoPDV.java
@@ -5,19 +5,50 @@
 package br.unipar.trabalhoPDV;
 
 import br.unipar.trabalhoPDV.Util.EntityManagerUtil;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 /**
  *
  * @author Beatr
  */
-public class TrabalhoPDV {
+public class TrabalhoPDV extends JFrame {
+    // Criando a barra de menu
+        JMenuBar menuBar = new JMenuBar();
+     //Menu de Exercícios
+        JMenu menuClientes = new JMenu("Clientes");
+        
+        
+    
+    
+    
+    
+    
+    
+    
+     private void abrirPanel(JPanel panel) {
+        getContentPane().removeAll();
+        getContentPane().add(panel);
+        revalidate();
+        repaint();
+    }      
 
     public static void main(String[] args) {
          SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-                   EntityManagerUtil.getEntityManagerFactory();
+                EntityManagerUtil.getEntityManagerFactory();
+                TrabalhoPDV menu = new TrabalhoPDV();
+                menu.setVisible(true);
             }
         });
     }
-}
+     @Override
+    public void dispose() {
+        EntityManagerUtil.closeEntityManagerFactory();
+        super.dispose(); 
+    }
+};
+
